9a093d2bc0ad0150389cd5abcfebb52d2b142778
Cremefreesh <archieeknight@gmail.com>
2026-08-29 22:11:57 +0100

    adding index so we can look up our orders and cancel quicker than o(N) brute force

--- include/order_book.hpp
+++ include/order_book.hpp
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <vector>
+#include <unordered_map>
 
 #include "order.hpp"
 #include "price_level.hpp"
@@ -11,10 +12,21 @@ class OrderBook {
 public:
     std::vector<Trade> addLimitOrder(Order order);
 
+    bool cancelOrder(OrderId id);
+
     Price bestBid() const;
     Price bestAsk() const;
 
 private:
+    struct OrderLocation {
+        Side side;
+        Price price;
+        std::list<Order>::iterator orderIt;
+    };
+
     std::map<Price, PriceLevel, std::greater<Price>> bids_;
     std::map<Price, PriceLevel> asks_;
+
+    std::unordered_map<OrderId, OrderLocation> orderIndex_;
 };
+

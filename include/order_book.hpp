c9d1d4b1d571ad3a933e1508633ddca4109caa89
Cremefreesh <archieeknight@gmail.com>
2026-08-29 21:15:19 +0100

    change to allow multiple trades from one order

--- include/order_book.hpp
+++ include/order_book.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <map>
+#include <vector>
 
 #include "order.hpp"
 #include "price_level.hpp"
+#include "trade.hpp"
 
 class OrderBook {
 public:
-    void addLimitOrder(Order order);
+    std::vector<Trade> addLimitOrder(Order order);
 
     Price bestBid() const;
     Price bestAsk() const;

8ac647f78e7e54d9b17c8867499a0af946d6d48a
Cremefreesh <archieeknight@gmail.com>
2026-09-23 01:13:58 +0100

    SPSC Buffer hpp setup

---
+++ include/SPSCQueue.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+#include <atomic>
+#include <cstddef>
+
+template <typename T, std::size_t Capacity>
+class SPSCQueue {
+public:
+    SPSCQueue() = default;
+
+    bool push(const T& value) {
+        // TODO
+        return false;
+    }
+
+    bool pop(T& value) {
+        // TODO
+        return false;
+    }
+
+    bool empty() const {
+        return true;
+    }
+
+private:
+    std::array<T, Capacity> buffer_{};
+
+    std::atomic<std::size_t> head_{0};
+    std::atomic<std::size_t> tail_{0};
+};

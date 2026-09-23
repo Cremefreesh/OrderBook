3fa5e9f7c398b5f3308a46446511880e2f5d37bf
Cremefreesh <archieeknight@gmail.com>
2026-09-23 01:38:48 +0100

    version 1 hpp bool push

--- include/SPSCQueue.hpp
+++ include/SPSCQueue.hpp
@@ -10,17 +10,40 @@ public:
     SPSCQueue() = default;
 
     bool push(const T& value) {
-        // TODO
-        return false;
-    }
+        const auto tail = tail_.load(std::memory_order_relaxed);
+        const auto next_tail = (tail + 1) % Capacity;
 
-    bool pop(T& value) {
-        // TODO
-        return false;
-    }
+        //full queue, cannot push
+        if (next_tail == head_.load(std::memory_order_acquire)) {
+            return false;
+        }
 
-    bool empty() const {
+        buffer_[tail] = value;
+
+        tail_.store(next_tail, std::memory_order_release);
+
         return true;
+    }
+
+
+
+    //autofilled by vs code --> check
+    bool pop(T& value) {
+        const auto head = head_.load(std::memory_order_relaxed);
+        const auto next_head = (head + 1) % Capacity;
+
+        if (next_head == tail_.load(std::memory_order_acquire)) {
+            return false;
+        }
+
+        value = buffer_[head];
+        head_.store(next_head, std::memory_order_release);
+        return true;
+    }
+
+    //autofilled by vs code --> check
+    bool empty() const {
+        return head_.load(std::memory_order_acquire) == tail_.load(std::memory_order_acquire);
     }
 
 private:
@@ -29,6 +52,14 @@ private:
 
     //head = next element the consumer reads
     //tail = next position the producer writes
+    //head for removing, tail for inserting
     std::atomic<std::size_t> head_{0};
     std::atomic<std::size_t> tail_{0};
 };
+
+
+//             WRITES              READS
+
+//head_        consumer            producer
+
+//tail_        producer            consumer
